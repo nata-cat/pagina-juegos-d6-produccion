e2700673b2de563125ffeac624bd99a025dfb6a6
natacat <nataly.jimenez.g@gmail.com>
2023-07-12 21:41:28 -0400

    primer commit

---
+++ js/about.6b56f342.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpagina_juegos_d6"]=self["webpackChunkpagina_juegos_d6"]||[]).push([[443],{782:function(n,a,u){u.r(a),u.d(a,{default:function(){return f}});var e=u(252);const s={class:"about"},t=(0,e._)("h1",null,"This is an about page",-1),r=[t];function i(n,a){return(0,e.wg)(),(0,e.iD)("div",s,r)}var c=u(744);const o={},d=(0,c.Z)(o,[["render",i]]);var f=d}}]);
+//# sourceMappingURL=about.6b56f342.js.map
